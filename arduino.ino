f79dc685b59025751535bb7e46c773aef24519c1
bromine1 <70975324+bromine1@users.noreply.github.com>
2022-03-08 08:10:54 -0500

    Small update to initialize libreary

--- arduino.ino
+++ arduino.ino
@@ -1,16 +1,24 @@
 #include <dht.h>
+#include <PulseSensorPlayground.h>
 #define dhtTemp A0 // Analog Pin sensor is connected to
-
+PulseSensorPlayground pulseSensor; //Give the module a bertter name
 dht temp; //Defines dht as DHT. Think import dht as DHT
+
 int buzzer = 12;//the pin of the active buzzer
+const int PulseWire = 0;
+int Threashold = 550;
+
 
 void setup()
 // init function, all the things that happen once
 {
-  //Initialize Serial for diagnostics
+    //Initialize Serial for diagnostics
     Serial.begin(9600);
     delay(500);//Delay to let system boot
 
+    pulseSensor.setThreshold(Threashold); // Define heartbeat threashold
+    //Initialize pins as output
+    pulseSensor.analogInput(PulseWire);
     pinMode(buzzer, OUTPUT); //initialize the buzzer pin as an output
 }
 
@@ -18,10 +26,9 @@ void loop()
 //think of this as the main function, continues to execute
 {
     temp.read11(dhtTemp);
-    if (temp.temperature > 100) 
+    
+    if (temp.temperature > 100 ) 
     {
-        Serial.print(temp.temperature);
-        Serial.print("Activating Buzzer \n");
         digitalWrite(buzzer, LOW);
         delay(500);
         noTone(buzzer);
